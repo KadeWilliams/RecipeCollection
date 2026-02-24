3391611ee568f42f32fe299440d8904b79f15348
kadewilliams <kadewilliams0@gmail.com>
2026-02-23 23:09:42 -0600

    updating

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
 
-  const apiTarget = isProd 
-    ? 'https://recipecollectionapi-production.up.railway.app/' 
-    : 'https://localhost:7014'
+  const apiTarget = isProd
+    ? 'https://recipecollectionapi-production.up.railway.app/'
+    : 'http://localhost:7014'
   return {
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      //'/api': {
-      //  target: 'https://localhost:7014/api/recipes',
-      //  changeOrigin: true,
-      //  rewrite: (path) => path.replace(/^\/api/,'')
-      //}
-      "/api": {
-        target: apiTarget,
-        changeOrigin: true,
-        secure: false,
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        //'/api': {
+        //  target: 'https://localhost:7014/api/recipes',
+        //  changeOrigin: true,
+        //  rewrite: (path) => path.replace(/^\/api/,'')
+        //}
+        "/api": {
+          target: apiTarget,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-}
+  }
 }
 );
