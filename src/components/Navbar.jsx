a008d7153b535bb865c323a0ccbb4c17da9da8db
Kade K. Williams <kade.williams@hobbylobby.com>
2026-02-27 13:46:05 -0600

    removed large comments of code that were unnecessary

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,3 @@
-/*import React from 'react'
-import { Link } from 'react-router-dom'
-
-const Navbar = () => {
-  return (
-    <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
-      <Link to="/" style={{alignSelf: 'center'}}>Recipe Collection</Link>
-      <ul>
-        <li style={{listStyle: 'none'}}><Link to="/recipes/add">Add Recipe</Link></li>
-      </ul>
-    </div>
-  )
-}
-
-export default Navbar*/
-
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
--- src/pages/EditRecipePage.jsx
+++ src/pages/EditRecipePage.jsx
@@ -1,21 +1,3 @@
-/*
-import { useLoaderData, Link } from "react-router-dom"
-import { editRecipe } from "../actions/recipe.actions";
-import RecipeForm from "../components/RecipeForm";
-
-const EditRecipePage = () => {
-  const recipe = useLoaderData();
-
-  return (
-    <>
-      <Link to="/">Back to Recipes</Link>
-      <RecipeForm recipe={recipe} recipeFunction={editRecipe}/>
-    </>
-  )
-}
-
-export default EditRecipePage
-*/
 import { useLoaderData, Link } from "react-router-dom";
 import { editRecipe } from "../actions/recipe.actions";
 import RecipeForm from "../components/RecipeForm";
